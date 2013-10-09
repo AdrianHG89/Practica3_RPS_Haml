5842d9478c1c94185568e845ca488ba8b9ee7d07
Adrian <adrihg89@gmail.com>
2013-10-09 10:34:32 +0100

    Indexando y añadiendo require en rps.rb

--- lib/rps.rb
+++ lib/rps.rb
@@ -1,53 +1,43 @@
 require 'rack/request'
 require 'rack/response'
+require 'haml'
+require 'thin'
+require 'rack'
   
 module RockPaperScissors
-   class App 
- 
-     def initialize(app = nil)
-       @app = app
-       @content_type = :html
-       @defeat = {'rock' => 'scissors', 'paper' => 'rock', 'scissors' => 'paper'}
-       @throws = @defeat.keys
-       @choose = @throws.map { |x| 
-          %Q{ <li><a href="/?choice=#{x}">#{x}</a></li> }
-       }.join("\n")
-       @choose = "<p>\n<ul>\n#{@choose}\n</ul>"
-     end
+	class RPS 
+ 		def initialize(app = nil)
+			@app = app
+			@content_type = :html
+			@defeat = {'rock' => 'scissors', 'paper' => 'rock', 'scissors' => 'paper'}
+			@throws = @defeat.keys
+			@choose = @throws.map { |x| 
+				%Q{ <li><a href="/?choice=#{x}">#{x}</a></li> }
+			}.join("\n")
+       			@choose = "<p>\n<ul>\n#{@choose}\n</ul>"
+		end #Ende del def initialize
   
-      def call(env)
-       req = Rack::Request.new(env)
- 
-        req.env.keys.sort.each { |x| puts "#{x} => #{req.env[x]}" }
-  
-       computer_throw = @throws.sample
-        player_throw = req.GET["choice"]
-        anwser = if !@throws.include?(player_throw)
-            "Choose one of the following:"
-          elsif player_throw == computer_throw
-            "You tied with the computer"
-          elsif computer_throw == @defeat[player_throw]
-            "Nicely done; #{player_throw} beats #{computer_throw}"
-          else
-            "Ouch; #{computer_throw} beats #{player_throw}. Better luck next time!"
-          end
-  
-        res = Rack::Response.new
-        res.write <<-"EOS"
-        <html>
-          <title>rps</title>
-          <body>
-            <h1>
-               #{anwser}
-               #{@choose}
-            </h1>
-          </body>
-        </html>
-        EOS
-       res.finish
-    end # call
-   end   # App
-end     # RockPaperScissors
+		def call(env)
+			req = Rack::Request.new(env)
+ 			req.env.keys.sort.each { |x| puts "#{x} => #{req.env[x]}" }
+			computer_throw = @throws.sample
+			player_throw = req.GET["choice"]
+			anwser =
+				if !@throws.include?(player_throw)
+					"Choose one of the following:"
+				elsif player_throw == computer_throw
+					"You tied with the computer"
+				elsif computer_throw == @defeat[player_throw]
+					"Nicely done; #{player_throw} beats #{computer_throw}"
+				else
+					"Ouch; #{computer_throw} beats #{player_throw}. Better luck next time!"
+				end
+			engine = Haml::Engine.new File.open("views/index.html.haml").read
+			res = Rack::Response.new
+			res.finish
+		end # End del def call
+	end #End de class
+end #End del module
   
 if $0 == __FILE__
     require 'rack'
